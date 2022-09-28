e7927e6686d47ed26cc83e074d9d8ee6101c0a4a
chanchal26 <chanchal.mch2613@gmail.com>
2022-09-28 21:06:43 +0600

    folder structure done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='home'>
+
+            <div className='left-side'>
+                <h1>i am from Home</h1>
+                <h2>i am from card</h2>
+            </div>
+            <div className='right-side'>
+                <h2>i am from cart</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
